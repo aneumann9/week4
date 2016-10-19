5ce9ae66838f73f91d60ae5be900ce66f7e860c6
aneumann9 <aneumann2017@kellogg.northwestern.edu>
2016-10-19 15:03:34 -0500

    Week 4 hwk first upload

---
+++ todo/index.js
@@ -0,0 +1,12 @@
+// This code runs when the page loads
+$(function() {
+
+  $(".button").on("click", function(event) {
+    event.preventDefault()
+    var elementThatWasClicked = $(this)
+    console.log(elementThatWasClicked)
+    elementThatWasClicked.remove()
+    // remove the element
+  })
+
+})
